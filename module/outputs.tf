0d7e347d212c1dca53872288121b354ee436b390
Gerric Chaplin <1778675+gerricchaplin@users.noreply.github.com>
2022-10-19 16:58:12 +0100

    TRIVIAL - Consolidate various changes across forks to one PR

--- module/outputs.tf
+++ module/outputs.tf
@@ -1,4 +1,4 @@
 output "lambda_arn" {
-  value       = "${aws_lambda_function.basic_auth.qualified_arn}"
+  value       = length(aws_lambda_function.basic_auth[*].qualified_arn) > 0 ? aws_lambda_function.basic_auth[0].qualified_arn : null 
   description = "Lambda function ARN with version"
 }
