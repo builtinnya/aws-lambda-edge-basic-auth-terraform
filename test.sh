e799805fdf8bf981551966ae89ff05663a74adcf
Assem Hasna <asem.hasna@gmail.com>
2023-08-25 15:40:01 +0200

    fix: tests

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Backup the file
+cp src/basic-auth.js src/basic-auth.js_backup
+
+# Perform the substitution using a different delimiter
+sed -i '' 's/\${hashed_username}/XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=/g' src/basic-auth.js
+sed -i '' 's/\${hashed_password}/BPiZbadjt6lpsQKO4wB1aerzpjVIbdqyEdUSyFud+Ps=/g' src/basic-auth.js
+
+# Run npm test
+npx mocha
+
+# Revert the changes
+mv src/basic-auth.js_backup src/basic-auth.js
---
+++ test_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Backup the file
+cp /codes/src/basic-auth.js /codes/src/basic-auth.js_backup
+
+# Perform the substitution using a different delimiter
+sed -i 's/\${hashed_username}/XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=/g' /codes/src/basic-auth.js
+sed -i 's/\${hashed_password}/BPiZbadjt6lpsQKO4wB1aerzpjVIbdqyEdUSyFud+Ps=/g' /codes/src/basic-auth.js
+
+# Run npm test
+npx mocha
+
+# Revert the changes
+mv /codes/src/basic-auth.js_backup /codes/src/basic-auth.js
